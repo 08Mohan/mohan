b1cd661696e1069a428278f6409362aef47731c7
08Mohan <mohan.pannirselvam@gmail.com>
2016-03-15 15:26:47 +0100

    Added files via upload

---
+++ server2.js
@@ -0,0 +1,23 @@
+var noble = require('noble');
+
+
+var socket = require('socket.io-client')('http://localhost/scanner');
+
+
+var addressToTrack = '6362ea74ba36';
+
+
+socket.on('connect', function () {
+    console.log('connected to server');
+});
+
+
+noble.on('discover', function (peripheral)
+ {
+     if (peripheral.uuid == addressToTrack)
+      {
+        socket.emit('deviceData', { mac: peripheral.uuid, rssi: peripheral.rssi });
+      }
+ });
+
+ noble.startScanning([], true);
