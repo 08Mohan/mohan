74f243e8462b4ef5a74dcc7e3057f5cb0d9e263d
08Mohan <mohan.pannirselvam@gmail.com>
2016-03-15 15:38:02 +0100

    Added files via upload

---
+++ scan3.js
@@ -0,0 +1,53 @@
+var noble = require('noble');
+
+
+var socket = require('socket.io-client')('http://localhost/scanner');
+
+
+var addressToTrack = '6362ea74ba36';
+
+socket.on('connect', function () {
+    console.log('connected to server');
+});
+
+
+console.log('noble');
+
+
+
+noble.on('stateChange', function (state) 
+{
+    console.log('on -> stateChange: ' + state);
+
+    if (state === 'poweredOn')
+    {
+        noble.startScanning();
+    }
+    else
+    {
+        noble.stopScanning();
+    }
+});
+
+
+
+noble.on('scanStart',function()
+{
+    console.log('on -> scanStart');
+});
+
+
+noble.on('scanstop',function()
+{
+    console.log('on -> scanStop');
+});
+
+
+noble.on('discover', function (peripheral)
+ {
+     if (peripheral.uuid == addressToTrack)
+      {
+        socket.emit('deviceData', { mac: peripheral.uuid, rssi: peripheral.rssi });
+      }
+ });
+    
