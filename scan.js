bd3a3398411720c223d24d11f0266f07c6f2bfb6
08Mohan <mohan.pannirselvam@gmail.com>
2016-03-15 16:10:15 +0100

    Update scan.js

--- scan.js
+++ scan.js
@@ -40,3 +40,19 @@ noble.on('discover', function (peripheral)
      var rssi = peripheral.rssi;
      console.log('found a device with macAddress:', macAddress, ' ', 'with a signal strength of :', ' ', rssi);
 });
+
+
+var socket = require('socket.io-client')('http://localhost/scanner');
+
+var addressToTrack = macAddress;
+
+socket.on('connect', function(){  
+  console.log('connected to server');
+});
+
+
+noble.on('discover', function(peripheral){
+  if(peripheral.uuid == addressToTrack){
+    socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});    
+  }
+});
